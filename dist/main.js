f6270d5755a05458923fea8cbc534f8d584a70c8
Max <maxrostowoj@gmail.com>
2021-09-11 22:38:26 +0300

    add a home part of website and adaptive for this part

--- dist/main.js
+++ dist/main.js
@@ -45,7 +45,17 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_zero_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/zero.scss */ \"./scss/zero.scss\");\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../css/style.css */ \"./css/style.css\");\n/* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../scss/style.scss */ \"./scss/style.scss\");\n/* harmony import */ var _img_logo_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../img/logo.png */ \"./img/logo.png\");\n\r\n\r\n\r\n\r\n\r\n\r\nwindow.addEventListener('DOMContentLoaded', () => {\r\n    const headerBurger = document.querySelector('.header__burger'),\r\n          headerMenu = document.querySelector('.header__menu');\r\n\r\n    headerBurger.addEventListener('click', (e) => {\r\n        headerBurger.classList.toggle('active');\r\n        headerMenu.classList.toggle('active');\r\n    });\r\n});\n\n//# sourceURL=webpack:///./js/script.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_zero_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/zero.scss */ \"./scss/zero.scss\");\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../css/style.css */ \"./css/style.css\");\n/* harmony import */ var _scss_style_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../scss/style.scss */ \"./scss/style.scss\");\n/* harmony import */ var _img_logo_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../img/logo.png */ \"./img/logo.png\");\n/* harmony import */ var _img_parthner1_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../img/parthner1.png */ \"./img/parthner1.png\");\n/* harmony import */ var _img_parthner2_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../img/parthner2.png */ \"./img/parthner2.png\");\n/* harmony import */ var _img_parthner3_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../img/parthner3.png */ \"./img/parthner3.png\");\n/* harmony import */ var _img_parthner4_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../img/parthner4.png */ \"./img/parthner4.png\");\n/* harmony import */ var _img_arrow_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../img/arrow.png */ \"./img/arrow.png\");\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nwindow.addEventListener('DOMContentLoaded', () => {\r\n\r\n    // Menu Burger\r\n    const headerBurger = document.querySelector('.header__burger'),\r\n          headerMenu = document.querySelector('.header__menu');\r\n\r\n    headerBurger.addEventListener('click', (e) => {\r\n        headerBurger.classList.toggle('active');\r\n        headerMenu.classList.toggle('active');\r\n        document.body.classList.toggle('lock');\r\n    });\r\n\r\n\r\n});\n\n//# sourceURL=webpack:///./js/script.js?");
+
+/***/ }),
+
+/***/ "./img/arrow.png":
+/*!***********************!*\
+  !*** ./img/arrow.png ***!
+  \***********************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"img/arrow.png\";\n\n//# sourceURL=webpack:///./img/arrow.png?");
 
 /***/ }),
 
@@ -56,6 +66,46 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scs
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 eval("module.exports = __webpack_require__.p + \"img/logo.png\";\n\n//# sourceURL=webpack:///./img/logo.png?");
+
+/***/ }),
+
+/***/ "./img/parthner1.png":
+/*!***************************!*\
+  !*** ./img/parthner1.png ***!
+  \***************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"img/parthner1.png\";\n\n//# sourceURL=webpack:///./img/parthner1.png?");
+
+/***/ }),
+
+/***/ "./img/parthner2.png":
+/*!***************************!*\
+  !*** ./img/parthner2.png ***!
+  \***************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"img/parthner2.png\";\n\n//# sourceURL=webpack:///./img/parthner2.png?");
+
+/***/ }),
+
+/***/ "./img/parthner3.png":
+/*!***************************!*\
+  !*** ./img/parthner3.png ***!
+  \***************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"img/parthner3.png\";\n\n//# sourceURL=webpack:///./img/parthner3.png?");
+
+/***/ }),
+
+/***/ "./img/parthner4.png":
+/*!***************************!*\
+  !*** ./img/parthner4.png ***!
+  \***************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"img/parthner4.png\";\n\n//# sourceURL=webpack:///./img/parthner4.png?");
 
 /***/ })
 
--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,24 @@ import '../scss/zero.scss';
 import '../css/style.css';
 import '../scss/style.scss';
 import '../img/logo.png';
+import '../img/parthner1.png';
+import '../img/parthner2.png';
+import '../img/parthner3.png';
+import '../img/parthner4.png';
+import '../img/arrow.png';
 
 
 window.addEventListener('DOMContentLoaded', () => {
+
+    // Menu Burger
     const headerBurger = document.querySelector('.header__burger'),
           headerMenu = document.querySelector('.header__menu');
 
     headerBurger.addEventListener('click', (e) => {
         headerBurger.classList.toggle('active');
         headerMenu.classList.toggle('active');
+        document.body.classList.toggle('lock');
     });
+
+
 });
